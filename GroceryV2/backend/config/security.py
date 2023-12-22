d9ce0358671766c249cadd85bd968e40e116b617
Ayush Gupta <ag141020@gmail.com>
2023-12-22 20:30:09 +0530

    Remove Projects

--- GroceryV2/backend/config/security.py
+++
@@ -1,6 +0,0 @@
-from flask_security import Security, SQLAlchemyUserDatastore
-from models.database import db
-from models.models import User, Role
-
-user_datastore = SQLAlchemyUserDatastore(db, User, Role)
-security = Security()
--- Grocery_StoreV2/routes/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-
-
-routes = Blueprint('routes', __name__)
-
-
-@routes.route("/", methods=["GET"])
-def home():
-    return render_template("index.html")
